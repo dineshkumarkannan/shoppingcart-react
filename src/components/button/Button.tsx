d820ec2f64dcff0d9dd8b9ed2d71fce9d915a991
Dinesh Kumar Kannan <dineshmail.kannan@gmail.com>
2025-03-10 08:49:56 +0530

    initial project setup commit

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,9 @@
+const Button = () => {
+  return (
+    <button className="flex p-1.5 border-2 rounded-md hover:cursor-pointer">
+      Click Me
+    </button>
+  );
+};
+
+export default Button;
---
+++ src/pages/home/HomePage.tsx
@@ -0,0 +1,13 @@
+import Button from "../../components/button/Button";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl">Home Page</h1>
+      <p className="font-light">Welcome to the Home Page</p>
+      <Button />
+    </div>
+  );
+};
+
+export default HomePage;
